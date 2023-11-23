25d8130c2e9a59e71a609cf492eb5e243dfe57c1
james <james@gedilabs.com>
2023-11-23 13:49:32 -0800

    fix: setting s3 bucket env var at startup

--- miniwdl_aws/__init__.py
+++ miniwdl_aws/__init__.py
@@ -1,5 +1,6 @@
 # omicshub: remap environment variables
-os.environ["MINIWDL__AWS__S3_UPLOAD_FOLDER"] = os.getenv("S3_BUCKET_MINIWDL", None) 
+import os
+os.environ["MINIWDL__AWS__S3_UPLOAD_FOLDER"] = os.getenv("S3_BUCKET_MINIWDL", "") 
 
 from .batch_job import BatchJob, BatchJobNoEFS  # noqa: F401
 from .cli_run_s3upload import miniwdl_run_s3upload  # noqa: F401
