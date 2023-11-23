7605176642880f8fff8bf1b2110089d1dd2cc67e
james <james@gedilabs.com>
2023-11-23 12:07:05 -0800

    chore: remap s3 upload bucket env var

--- miniwdl_aws/__init__.py
+++ miniwdl_aws/__init__.py
@@ -1,3 +1,6 @@
+# omicshub: remap environment variables
+os.environ["MINIWDL__AWS__S3_UPLOAD_FOLDER"] = os.getenv("S3_BUCKET_MINIWDL", None) 
+
 from .batch_job import BatchJob, BatchJobNoEFS  # noqa: F401
 from .cli_run_s3upload import miniwdl_run_s3upload  # noqa: F401
 from .cli_submit import miniwdl_submit_awsbatch  # noqa: F401
